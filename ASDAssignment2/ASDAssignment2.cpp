fb60cc6a192421c93b2e26ed8c8a3680032a3006
omarhimada <contact@omarhimada.com>
2026-09-18 14:02:03 -0400

    pre-release compiler directive

--- ASDAssignment2/ASDAssignment2.cpp
+++ ASDAssignment2/ASDAssignment2.cpp
@@ -1,18 +1,32 @@
-#define BRANCH "main"
+#define PRE_RELEASE 1
 
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <iostream>
 
 struct Student {
 	std::string firstName;
 	std::string lastName;
+#if PRE_RELEASE
+	std::string email;
+#endif
 };
 
 int main() {
+#if PRE_RELEASE
+	std::ifstream file("StudentData_Emails.txt");
+#else
 	std::ifstream file("StudentData.txt");
+#endif
+
+	#if PRE_RELEASE
+		std::cout << "(Pre-relase branch)";
+	#else
+		std::cout << "(Main branch)";
+	#endif
 
 	if (!file) {
 		throw std::runtime_error("Failed to open StudentData.txt");
@@ -29,13 +43,27 @@ int main() {
 		std::stringstream ss(line);
 		std::string lastName;
 		std::string firstName;
-
+#if PRE_RELEASE
+		std::string email;
+#endif
+#if PRE_RELEASE
 		if (std::getline(ss, lastName, ',') &&
-			std::getline(ss, firstName)) {
+			std::getline(ss, firstName, ',') &&
+			std::getline(ss, email)) {
+			students.push_back({
+				.firstName = firstName,
+				.lastName = lastName,
+				.email = email
+				});
+		}
+#else
+		if (std::getline(ss, lastName, ',') &&
+			std::getline(ss, firstName, ',')) {
 			students.push_back({
 				.firstName = firstName,
 				.lastName = lastName
 				});
 		}
+#endif
 	}
 }
